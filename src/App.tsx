a65f57e7b0f2413a1feb8c1320fa8c6cbc933cd0
zeratul1215 <ztianhong7@gmail.com>
2025-06-17 14:32:03 +0900

    fix: useTitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router';
 import AppRoutes from '@/routes/AppRoutes';
-import { HelmetProvider } from 'react-helmet-async';
 
 const App: React.FC = () => {
   return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 };
 
