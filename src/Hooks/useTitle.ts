d75d13034ac5c242fa108e537013a9650f57d3e1
zeratul1215 <ztianhong7@gmail.com>
2025-06-18 11:54:35 +0900

    fix: delete old Hooks

--- src/Hooks/useTitle.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export function useTitle(title: string) {
-  useEffect(() => {
-    if (title.trim().length > 0) {
-      document.title = title.trim();
-    }
-  }, [title]);
-}
